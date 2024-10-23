488935ca72f2b151710a521c7e9975a8cfc7581c
nazmul.sourab <nazmul.sourab@technonext.com>
2024-10-23 15:42:21 +0600

    update code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Create a status bar item
+  const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  statusBarItem.text = "Create MR"; // Add icon and label
+  statusBarItem.tooltip = "Click to open Merge Request page"; // Tooltip when hovering over the icon
+  statusBarItem.command = "extension.openMergeRequest"; // Command to trigger
+  statusBarItem.show(); // Display the status bar item
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gitlab--create-mr" is now active!');
+  // Register the command
+  let disposable = vscode.commands.registerCommand(
+    "extension.openMergeRequest",
+    async () => {
+      // Retrieve settings from the configuration
+      const config = vscode.workspace.getConfiguration("mergeRequest");
+      const projectId = config.get<string>("projectId");
+      const sourceBranchName = config.get<string>("sourceBranchName");
+      const targetBranchName = config.get<string>("targetBranchName", "main");
+      const gitlabDomain = config.get<string>("gitlabDomain", "https://gitlab.com");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('gitlab--create-mr.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GitLab: Create MR!');
-	});
+      // Check if the required fields are set in settings
+      if (!projectId || !sourceBranchName) {
+        vscode.window.showErrorMessage("Missing required settings.");
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      // Construct the URL for the merge request page
+      const mergeRequestUrl = `${gitlabDomain}/-/merge_requests/new?merge_request[source_project_id]=${projectId}&merge_request[source_branch]=${sourceBranchName}&merge_request[target_project_id]=${projectId}&merge_request[target_branch]=${targetBranchName}`;
+
+      // Open the merge request page in the browser
+      await vscode.env.openExternal(vscode.Uri.parse(mergeRequestUrl));
+    }
+  );
+
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(statusBarItem); // Add status bar item to subscriptions
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
